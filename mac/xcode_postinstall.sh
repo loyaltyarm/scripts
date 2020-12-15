b5d18c47eeb762867292bbf7adf7ed0f83da997d
loyaltyarm <loyaltyarm@gmail.com>
2020-12-14 23:15:42 -0800

    update xcode script with core types installer - forgot to commit

--- mac/xcode_postinstall.sh
+++ mac/xcode_postinstall.sh
@@ -24,6 +24,11 @@ if [[ -e "/Applications/Xcode.app/Contents/Resources/Packages/XcodeSystemResourc
   /usr/sbin/installer -dumplog -verbose -pkg "/Applications/Xcode.app/Contents/Resources/Packages/XcodeSystemResources.pkg" -target /
 fi
 
+# Install Core Types Package, fourth package in 'installing components' dialog when opening Xcode
+if [[ -e "/Applications/Xcode.app/Contents/Resources/Packages/CoreTypes.pkg" ]]; then
+  /usr/sbin/installer -dumplog -verbose -pkg "/Applications/Xcode.app/Contents/Resources/Packages/CoreTypes.pkg" -target /
+fi
+
 # Enable usage of developer tools
 sudo DevToolsSecurity -enable
 
